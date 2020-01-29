5835ac791455fc153a3e8695b71dc37edd54cdf5
ElenaSezionova <elena.sezionova@gmail.com>
2020-01-29 11:53:55 -0500

    Python Challenges

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9cfaeb3dbd4832137f0fb5f0e12ca0f4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBwiWBdcMksrxnWzcOvCM1cjxhqV8017_A"
